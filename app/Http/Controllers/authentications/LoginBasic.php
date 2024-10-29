17d17710313551dbdb3e4300ebc106a9a2e0e76b
沈-98 <73530108+enricho98@users.noreply.github.com>
2024-10-29 22:34:22 +0700

    fixing bug on login

--- app/Http/Controllers/authentications/LoginBasic.php
+++ app/Http/Controllers/authentications/LoginBasic.php
@@ -3,10 +3,8 @@
 namespace App\Http\Controllers\authentications;
 
 use App\Http\Controllers\Controller;
-use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Spatie\Permission\Models\Role;
 
 class LoginBasic extends Controller
 {
@@ -55,7 +53,6 @@ class LoginBasic extends Controller
 
     public function login(Request $request)
     {
-        // Validate input
         $request->validate([
             'email' => 'required|string',
             'password' => 'required|string',
@@ -64,16 +61,14 @@ class LoginBasic extends Controller
         // Retrieve input
         $input = $request->only('email', 'password');
 
+        // Determine if input is an email or username
+        $field = filter_var($input['email'], FILTER_VALIDATE_EMAIL) ? 'email' : 'name';
+
+        // Add the field to the input array
+        $credentials = [$field => $input['email'], 'password' => $input['password']];
+
         // Attempt authentication
-        if (Auth::attempt($input)) {
-            $user = Auth::user();
-
-            // Cek dan assign role admin
-            $role = Role::where('name', 'admin')->first();
-            if ($role) {
-                $user->assignRole($role);
-            }
-
+        if (Auth::attempt($credentials)) {
             return redirect()->route('dashboard.analytics');
         }
 
